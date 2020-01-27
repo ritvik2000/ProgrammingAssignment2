87b805143c9183cc0afdfaa0c8453f1a14542533
Ritvik Goyal <59802195+ritvik2000@users.noreply.github.com>
2020-01-28 01:54:59 +0530

    Assignment Completion

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,15 @@
 ## A pair of functions that cache the inverse of a matrix
 
 ## Creatws a matrix that is able to cache it's inverse
+## makeCacheMatrix: this function creates a cacheMatrix from the first
+##
+## The cacheMatrix has the following methods:
+##
+## cacheMatrix$get(): gets the contents as a regular matrix
+## cacheMatrix$set(y): sets the contents to the matrix y, resetting the cache
+##
+## cacheMatrix$p.getInverse(): gets the cached inverse; NULL if no inverse is set
+## cacheMatrix$p.setInverse(new_inv): set the inverse cache
 
 makeCacheMatrix <- function(x = matrix()) {
            i <- NULL
@@ -19,6 +28,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Compute the inverse of the Cached matrix
+## This returns the inverse of the cacheMatrix. As long as the matrix has 
+## not been changed between calls, successive calls to cacheSolve(m) 
+## would not result in re-computation of the inverse (using the intrinsic)
+## cache.
+##
+## Note: all parameters other than x (the cacheMatrix) are ignored given they
+## have not been mentioned in the assignment. If we pass them to solve(),
+## then we need to check when we retrieve the cache whether they had been 
+## the same when the cache was first computed
 cacheSolve <- function(x, ...) { 
        
         i <- x$getinverse()
